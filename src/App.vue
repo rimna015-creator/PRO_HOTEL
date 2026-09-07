25cf551dd304f2454388e9a7bc81ae40145e797c
rimna015-creator <rimna015@gmail.com>
2026-09-07 11:30:29 +0700

    add create Account

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Login from './components/Login.vue';
+import CreateAccount from './components/CreateAccount.vue';
 
 
 
@@ -8,11 +9,18 @@ const showLogin=ref(true)
 const loginSuccess=()=>{
   showLogin.value=false
 }
+const showCreateAccount=ref(false)
+const gotocreateAccount=()=>{
+  showLogin.value=false
+  showCreateAccount.value=true
+}
 </script>
 
 <template>
   <!-- Login Popup-->
  <Login v-if="showLogin"
-    @login-success="loginSuccess"/>
- 
+    @login-success="loginSuccess"
+    @create-account="showCreateAccount=true"/>
+ <CreateAccount v-if="showCreateAccount"
+  @back-to-login="showCreateAccount=false"/>
 </template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
 <!-- Form Area -->
-<div class="space-y-8 p-8 sm:p-12">
+<div class="space-y-5 p-6 sm:p-8">
+  <div class="text-2xl text-center font-bold mb-1">Welcome back</div>
+  <div class="text-2xl text-center font-bold">Sign In</div>
   <!-- Email Field -->
   <div>
     <label for="email" class="block text-base font-semibold text-zinc-900">
@@ -12,7 +14,7 @@
         v-model="email"
         type="email"
         placeholder="you@email.com"
-        class="block w-full rounded-xl border border-zinc-200 bg-zinc-50 px-5 py-4 text-zinc-900 placeholder:text-zinc-400 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-100 sm:text-lg"
+        class="block w-full rounded-xl border border-zinc-200 bg-zinc-50 px-4 py-3 text-zinc-900 placeholder:text-zinc-400 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-100 "
       />
     </div>
   </div>
@@ -23,7 +25,7 @@
       <label for="password" class="block text-base font-semibold text-zinc-900">
         Password
       </label>
-      <a href="#" class="text-sm font-medium text-blue-600 hover:text-blue-700">
+      <a class="text-sm font-medium text-blue-600 hover:text-blue-700">
         Forgot password?
       </a>
     </div>
@@ -33,7 +35,7 @@
         v-model="password"
         type="password"
         placeholder="••••••••••••"
-        class="block w-full rounded-xl border border-zinc-200 bg-zinc-50 px-5 py-4 text-zinc-900 placeholder:text-zinc-400 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-100 sm:text-lg"
+        class="block w-full rounded-xl border border-zinc-200 bg-zinc-50 px-4 py-3 text-zinc-900 placeholder:text-zinc-400 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-100 "
       />
     </div>
   </div>
@@ -42,9 +44,9 @@
   <div class="pt-2">
     <button
       @click="login"
-      class="flex w-full items-center justify-center rounded-2xl bg-blue-600 px-8 py-4 text-lg font-bold text-white shadow-lg shadow-blue-500/20 transition-all hover:bg-blue-700 hover:shadow-blue-500/30 focus:outline-none focus:ring-4 focus:ring-blue-200 active:scale-[0.98]"
+      class="flex w-full items-center justify-center rounded-2xl bg-blue-600 px-6 py-3 font-bold text-white shadow-lg shadow-blue-500/20 transition-all hover:bg-blue-700 hover:shadow-blue-500/30 focus:outline-none focus:ring-4 focus:ring-blue-200 active:scale-[0.98]"
     >
-      Sign In to Your Account
+      Login In to Your Account
     </button>
   </div>
 
@@ -52,9 +54,10 @@
   <div class="pt-4 text-center">
     <p class="text-zinc-600">
       Don't have an account yet? 
-      <a href="#" class="font-semibold text-blue-600 hover:text-blue-700">
-        Create one for free
-      </a>
+      <button
+      @click="gotocreateAccount"
+      class="font-semibold text-blue-600 hover:text-blue-700"
+      >Create one for free</button>
     </p>
   </div>
 </div>
@@ -62,7 +65,6 @@
 
 
 <script setup lang="ts">
-
 import { ref } from 'vue'
 
 const email = ref('')
@@ -79,6 +81,6 @@ const login = () => {
   emit('login-success')
 }
 
-const emit = defineEmits(['login-success'])
-
+const emit = defineEmits(['login-success','create-account'])
+const gotocreateAccount=()=>{emit('create-account')}
 </script>
